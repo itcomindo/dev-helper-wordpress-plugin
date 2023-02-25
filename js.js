19070eca5d441dc73715f02c3b87744ec66ecd0a
itcomindo <itcomindo@gmail.com>
2023-02-25 23:22:36 +0700

    update

--- js.js
+++ js.js
@@ -1,6 +1,6 @@
 window.addEventListener('DOMContentLoaded', (event) => {
     jQuery(function () {
-        var devShortcuts = jQuery('.devShortcuts.inactive');
+        var devShortcuts = jQuery('.devShortcuts');
         // var devContent = jQuery('#devContent.inactive');
         devShortcuts.slideUp();
         var devTrigger = jQuery('.devTrigger');
@@ -9,15 +9,20 @@ window.addEventListener('DOMContentLoaded', (event) => {
         var devHelperPr = jQuery('#devHelperPr');
         toggleOpenDH.slideUp();
         var inactive = localStorage.getItem('devHelperPr');
-
-        // storage start
-        // storage end
-
+        
+        // run open accordion
         jQuery(devTrigger).click(function () {
-            jQuery(devShortcuts).slideUp();
+            devShortcuts.slideUp();
+            if (jQuery('.devshortcuts').hasClass('active')) {
+                jQuery('.devshortcuts').removeClass('active').addClass('inactive');
+            }
             var devTriggerAttr = jQuery(this).attr('data-toggle');
-            jQuery(this).next('#' + devTriggerAttr).slideDown();
-            jQuery(toggleOpenDH).slideUp();
+            var currentActive = jQuery(this).next('#' + devTriggerAttr + '.devShortcuts');
+            if (currentActive.hasClass('inactive')) {
+                jQuery(currentActive).removeClass('inactive').addClass('active').slideDown();
+            } else {
+                jQuery('.devShortcuts').removeClass('active').addClass('inactive').slideUp();
+            }
         });
 
         if (inactive == 'inactive') {
@@ -25,7 +30,6 @@ window.addEventListener('DOMContentLoaded', (event) => {
         } else {
             jQuery(devHelperPr).addClass('active').removeClass('inactive');
         }
-        
         // close workspace
         jQuery(toggleCloseDH).click(function () {
             jQuery(devShortcuts).slideUp();
@@ -33,9 +37,6 @@ window.addEventListener('DOMContentLoaded', (event) => {
             jQuery(toggleOpenDH).slideDown();
             jQuery(devHelperPr).addClass('inactive').removeClass('active');
             localStorage.setItem('devHelperPr', 'inactive');
-            
-
-            
         });
         // open workspace
         jQuery(toggleOpenDH).click(function () {
--- workspace.php
+++ workspace.php
@@ -5,111 +5,147 @@ function dev_helper_workspace()
 {
     // if user is administrator and logged in
     if (current_user_can('administrator') && is_user_logged_in()) {
-    
 
 
-    $blog = get_bloginfo('url');
-    $wpadmin = $blogUrl . '/wp-admin';
-    $allpost = $wpadmin . '/edit.php';
-    $newpost = $wpadmin . '/post-new.php';
-    $allpage = $wpadmin . '/edit.php?post_type=page';
-    $newpage = $wpadmin . '/post-new.php?post_type=page';
-    $medialibrary = $wpadmin . '/upload.php';
-    $uploadmedia = $wpadmin . '/media-new.php';
-    $alltags = $wpadmin . '/edit-tags.php?taxonomy=post_tag';
-    $newtag = $wpadmin . '/edit-tags.php?action=add&taxonomy=post_tag';
-    $allcategories = $wpadmin . '/edit-tags.php?taxonomy=category';
-    $newcategory = $wpadmin . '/edit-tags.php?action=add&taxonomy=category';
-    $allplugins = $wpadmin . '/plugins.php';
-    $newplugin = $wpadmin . '/plugin-install.php';
-    $allthemes = $wpadmin . '/themes.php';
-    $newtheme = $wpadmin . '/theme-install.php';
-    $themefileditor = $wpadmin . '/theme-editor.php';
-    $navmenus = $wpadmin . '/nav-menus.php';
-    $newmenus = $wpadmin . '/nav-menus.php?action=edit&menu=0';
-    $plugineditor = $wpadmin . '/plugin-editor.php';
-    $widgets = $wpadmin . '/widgets.php';
-    $comments = $wpadmin . '/edit-comments.php';
-    $settings = $wpadmin . '/options-general.php';
-    $reading = $wpadmin . '/options-reading.php';
-    $writing = $wpadmin . '/options-writing.php';
-    $discussion = $wpadmin . '/options-discussion.php';
-    $mediasettings = $wpadmin . '/options-media.php';
-    $permalinks = $wpadmin . '/options-permalink.php';
-    $users = $wpadmin . '/users.php';
-    $newuser = $wpadmin . '/user-new.php';
-    $profile = $wpadmin . '/profile.php';
-    $tools = $wpadmin . '/tools.php';
-    $import = $wpadmin . '/import.php';
-    $export = $wpadmin . '/export.php';
-    $sitehealth = $wpadmin . '/site-health.php';
-    $sitehealthInfo = $wpadmin . '/site-health.php?tab=info';
-    $checkupdates = $wpadmin . '/update-core.php';
-    //create logout button when click logout then redirect to home page
-    $logout = wp_logout_url(home_url());
-    
+
+        $blog = get_bloginfo('url');
+        $wpadmin = $blogUrl . '/wp-admin';
+        $allpost = $wpadmin . '/edit.php';
+        $newpost = $wpadmin . '/post-new.php';
+        $editpost = $wpadmin . '/post.php?post=' . get_the_ID() . '&action=edit';
+        $allpage = $wpadmin . '/edit.php?post_type=page';
+        $newpage = $wpadmin . '/post-new.php?post_type=page';
+        $medialibrary = $wpadmin . '/upload.php';
+        $uploadmedia = $wpadmin . '/media-new.php';
+        $alltags = $wpadmin . '/edit-tags.php?taxonomy=post_tag';
+        $newtag = $wpadmin . '/edit-tags.php?action=add&taxonomy=post_tag';
+        $allcategories = $wpadmin . '/edit-tags.php?taxonomy=category';
+        $newcategory = $wpadmin . '/edit-tags.php?action=add&taxonomy=category';
+        $allplugins = $wpadmin . '/plugins.php';
+        $newplugin = $wpadmin . '/plugin-install.php';
+        $allthemes = $wpadmin . '/themes.php';
+        $newtheme = $wpadmin . '/theme-install.php';
+        $themefileditor = $wpadmin . '/theme-editor.php';
+        $navmenus = $wpadmin . '/nav-menus.php';
+        $newmenus = $wpadmin . '/nav-menus.php?action=edit&menu=0';
+        $plugineditor = $wpadmin . '/plugin-editor.php';
+        $widgets = $wpadmin . '/widgets.php';
+        $comments = $wpadmin . '/edit-comments.php';
+        $settings = $wpadmin . '/options-general.php';
+        $reading = $wpadmin . '/options-reading.php';
+        $writing = $wpadmin . '/options-writing.php';
+        $discussion = $wpadmin . '/options-discussion.php';
+        $mediasettings = $wpadmin . '/options-media.php';
+        $permalinks = $wpadmin . '/options-permalink.php';
+        $users = $wpadmin . '/users.php';
+        $newuser = $wpadmin . '/user-new.php';
+        $profile = $wpadmin . '/profile.php';
+        $tools = $wpadmin . '/tools.php';
+        $import = $wpadmin . '/import.php';
+        $export = $wpadmin . '/export.php';
+        $sitehealth = $wpadmin . '/site-health.php';
+        $sitehealthInfo = $wpadmin . '/site-health.php?tab=info';
+        $checkupdates = $wpadmin . '/update-core.php';
+        //create logout button when click logout then redirect to home page
+        $logout = wp_logout_url(home_url());
+        if (is_single() || is_page()) {
+            $where = 'dhSingle';
+        } else {
+            $where = 'dhNonSingle';
+        }
+
 
 
 ?>
-    <div id="devHelperPr" class="active">
-        <div id="toggleCloseDH" class="active">Close</div>
-        <div id="toggleOpenDH" class="active">Open</div>
-        <div class="devHelpWr">
-            <!-- trigger start -->
-            <div class="devTriggerWr">
-                <div data-toggle="devwpadmin" class="devTrigger">Wp Admin</div>
-                <div id="devwpadmin" class="inactive devShortcuts">
-                    <ul>
-                        <li><a target="_blank" href="<?php echo $wpadmin . '/'; ?>">Wp Admin</a></li>
-                        <li><a target="_blank" href="<?php echo $logout; ?>">Logout</a></li>
-                    </ul>
-                </div>
-                <div data-toggle="devcontent" class="devTrigger">Content</div>
-                <div id="devcontent" class="inactive devShortcuts">
-                    <ul>
-                        <li><a target="_blank" href="<?php echo $allpost; ?>">All Posts</a></li>
-                        <li><a target="_blank" href="<?php echo $newpost; ?>">New Posts</a></li>
-                        <li><a target="_blank" href="<?php echo $allpage; ?>">All Pages</a></li>
-                        <li><a target="_blank" href="<?php echo $newpage; ?>">New Page</a></li>
-                        <li><a target="_blank" href="<?php echo $allcategories; ?>">All Categories</a></li>
-                        <li><a target="_blank" href="<?php echo $newcategory; ?>">New Category</a></li>
-                        <li><a target="_blank" href="<?php echo $alltags; ?>">All Tags</a></li>
-                        <li><a target="_blank" href="<?php echo $newtag; ?>">New Tag</a></li>
-                    </ul>
-                </div>
+        <div id="devHelperPr" class="active">
+            <div id="toggleCloseDH" class="active">Close</div>
+            <div id="toggleOpenDH" class="active">Open</div>
+            <div class="devHelpWr <?php echo $where; ?>">
+                <!-- trigger start -->
 
-                <div data-toggle="devthemes" class="devTrigger">Themes</div>
-                <div id="devthemes" class="inactive devShortcuts">
-                    <ul>
-                        <li><a target="_blank" href="<?php echo $allthemes; ?>">All Themes</a></li>
-                        <li><a target="_blank" href="<?php echo $newtheme; ?>">New Theme</a></li>
-                        <li><a target="_blank" href="<?php echo $navmenus; ?>">All Menu</a></li>
-                        <li><a target="_blank" href="<?php echo $newmenus; ?>">New Menu</a></li>
-                        <li><a target="_blank" href="<?php echo $themefileditor; ?>">Theme File Editor</a></li>
-                    </ul>
+                <div class="devTriggerWr">
+                    <div data-toggle="devwpadmin" class="devTrigger">Wp Admin</div>
+                    <div id="devwpadmin" class="devShortcuts inactive">
+                        <ul>
+                            <li><a target="_blank" href="<?php echo $wpadmin . '/'; ?>">Wp Admin</a></li>
+                            <li><a target="_blank" href="<?php echo $logout; ?>">Logout</a></li>
+                        </ul>
+                    </div>
+                    <div data-toggle="devcontent" class="devTrigger">Content</div>
+                    <div id="devcontent" class="devShortcuts inactive">
+                        <ul>
+                            <li><a target="_blank" href="<?php echo $allpost; ?>">All Posts</a></li>
+                            <li><a target="_blank" href="<?php echo $newpost; ?>">New Posts</a></li>
+                            <li><a target="_blank" href="<?php echo $allpage; ?>">All Pages</a></li>
+                            <li><a target="_blank" href="<?php echo $newpage; ?>">New Page</a></li>
+                            <li><a target="_blank" href="<?php echo $allcategories; ?>">All Categories</a></li>
+                            <li><a target="_blank" href="<?php echo $newcategory; ?>">New Category</a></li>
+                            <li><a target="_blank" href="<?php echo $alltags; ?>">All Tags</a></li>
+                            <li><a target="_blank" href="<?php echo $newtag; ?>">New Tag</a></li>
+                        </ul>
+                    </div>
+
+                    <div data-toggle="devthemes" class="devTrigger">Themes</div>
+                    <div id="devthemes" class="devShortcuts inactive">
+                        <ul>
+                            <li><a target="_blank" href="<?php echo $allthemes; ?>">All Themes</a></li>
+                            <li><a target="_blank" href="<?php echo $newtheme; ?>">New Theme</a></li>
+                            <li><a target="_blank" href="<?php echo $navmenus; ?>">All Menu</a></li>
+                            <li><a target="_blank" href="<?php echo $newmenus; ?>">New Menu</a></li>
+                            <li><a target="_blank" href="<?php echo $themefileditor; ?>">Theme File Editor</a></li>
+                        </ul>
+                    </div>
+                    <div data-toggle="devplugins" class="devTrigger">Plugins</div>
+                    <div id="devplugins" class="devShortcuts inactive">
+                        <ul>
+                            <li><a target="_blank" href="<?php echo $allplugins; ?>">All Plugins</a></li>
+                            <li><a target="_blank" href="<?php echo $newplugin; ?>">New Plugin</a></li>
+                            <li><a target="_blank" href="<?php echo $plugineditor; ?>">Plugin Editor</a></li>
+                        </ul>
+                    </div>
+                    <div data-toggle="devmedia" class="devTrigger">Media</div>
+                    <div id="devmedia" class="devShortcuts inactive">
+                        <ul>
+                            <li><a target="_blank" href="<?php echo $medialibrary; ?>">Media Library</a></li>
+                            <li><a target="_blank" href="<?php echo $uploadmedia; ?>">Upload New Media</a></li>
+                        </ul>
+                    </div>
                 </div>
-                <div data-toggle="devplugins" class="devTrigger">Plugins</div>
-                <div id="devplugins" class="inactive devShortcuts">
-                    <ul>
-                        <li><a target="_blank" href="<?php echo $allplugins; ?>">All Plugins</a></li>
-                        <li><a target="_blank" href="<?php echo $newplugin; ?>">New Plugin</a></li>
-                        <li><a target="_blank" href="<?php echo $plugineditor; ?>">Plugin Editor</a></li>
-                    </ul>
-                </div>
-                <div data-toggle="devmedia" class="devTrigger">Media</div>
-                <div id="devmedia" class="inactive devShortcuts">
-                    <ul>
-                        <li><a target="_blank" href="<?php echo $medialibrary; ?>">Media Library</a></li>
-                        <li><a target="_blank" href="<?php echo $uploadmedia; ?>">Upload New Media</a></li>
-                    </ul>
-                </div>
+                <!-- trigger end -->
+                <!-- shortcut start -->
+                <?php
+                if (is_single() || is_page()) {
+                    // $editpost = get_edit_post_link();
+                    // $editpost = '<a target="_blank" href="' . $editpost . '"><i class="fas fa-pencil-alt"></i>Edit Post</a>';
+                ?>
+                    <div class="shortCutWr">
+                        <!-- for post start -->
+                        <?php
+                        if (is_single()) {
+                            ?>
+                            <a href="<?php echo $editpost; ?>"><i class="fas fa-pencil-alt"></i>Edit Post</a>
+
+                            <a href="<?php echo $newpost; ?>"><i class="fas fa-pencil-alt"></i>New Post</a>
+                            <?php
+                        } elseif (is_page()) {
+                            ?>
+                            <a href="<?php echo $editpost; ?>"><i class="fas fa-pencil-alt"></i>Edit Page</a>
+                            
+                            <a href="<?php echo $newpage; ?>"><i class="fas fa-pencil-alt"></i>New Page</a>
+                        <?php
+                        }
+                        ?>
+                        <!-- for post end -->
+                        <!-- for page start -->
+                        <!-- for page end -->
+                    </div>
+                <?php
+                }
+                ?>
+
+                <!-- shortcut end -->
             </div>
-            <!-- trigger end -->
-            <!-- shortcut start -->
-            <div class="shortCutWr"></div>
-            <!-- shortcut end -->
         </div>
-    </div>
 
 <?php
     } else {
