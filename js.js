b3db03798b2c692a1578b55495646115cf3db862
itcomindo <itcomindo@gmail.com>
2023-03-08 22:40:23 +0700

    updat

--- js.js
+++ js.js
@@ -9,14 +9,50 @@ window.addEventListener('DOMContentLoaded', (event) => {
         var devHelperPr = jQuery('#devHelperPr');
         toggleOpenDH.slideUp();
         var inactive = localStorage.getItem('devHelperPr');
-        
+
         // run open accordion
-        jQuery(devTrigger).click(function () {
+        // jQuery(devTrigger).click(function () {
+        //     devShortcuts.slideUp();
+        //     if (jQuery('.devshortcuts').hasClass('active')) {
+        //         jQuery('.devshortcuts').removeClass('active').addClass('inactive');
+        //     }
+        //     var devTriggerAttr = jQuery(this).attr('data-toggle');
+        //     var currentActive = jQuery(this).next('#' + devTriggerAttr + '.devShortcuts');
+        //     if (currentActive.hasClass('inactive')) {
+        //         jQuery(currentActive).removeClass('inactive').addClass('active').slideDown();
+        //     } else {
+        //         jQuery('.devShortcuts').removeClass('active').addClass('inactive').slideUp();
+        //     }
+        // });
+
+        // code below is not working
+        // jQuery(devTrigger).hover(function () {
+        //     devShortcuts.slideUp();
+        //     if (jQuery('.devshortcuts').hasClass('active')) {
+        //         jQuery('.devshortcuts').removeClass('active').addClass('inactive');
+        //     }
+        //     var devTriggerAttr = jQuery(this).attr('data-toggle');
+        //     var currentActive = jQuery(this).next('#' + devTriggerAttr + '.devShortcuts');
+        //     if (currentActive.hasClass('inactive')) {
+        //         jQuery(currentActive).removeClass('inactive').addClass('active').slideDown();
+        //     } else {
+        //         jQuery('.devShortcuts').removeClass('active').addClass('inactive').slideUp();
+        //     }
+        // }, function () {
+        //     jQuery('.devShortcuts').removeClass('active').addClass('inactive').slideUp();
+        // });
+
+
+        // if mouse enter on devTrigger then open accordion and keep accordion open until mouse leave
+
+        jQuery(devTrigger).mouseenter(function () {
             devShortcuts.slideUp();
+            jQuery(this).addClass('test');
             if (jQuery('.devshortcuts').hasClass('active')) {
                 jQuery('.devshortcuts').removeClass('active').addClass('inactive');
             }
             var devTriggerAttr = jQuery(this).attr('data-toggle');
+            console.log(devTriggerAttr);
             var currentActive = jQuery(this).next('#' + devTriggerAttr + '.devShortcuts');
             if (currentActive.hasClass('inactive')) {
                 jQuery(currentActive).removeClass('inactive').addClass('active').slideDown();
@@ -25,11 +61,34 @@ window.addEventListener('DOMContentLoaded', (event) => {
             }
         });
 
+        // when mouse leave devTrigger but still on devShortcuts then keep accordion open
+        jQuery(devShortcuts).mouseenter(function () {
+            jQuery('.devTrigger.test').addClass('test2');
+        });
+
+        // when mouse leave devShortcuts then close accordion
+        jQuery(devShortcuts).mouseleave(function () {
+            jQuery('.devTrigger.test').removeClass('test');
+            jQuery('.devTrigger.test2').removeClass('test2');
+            jQuery('.devShortcuts').removeClass('active').addClass('inactive').slideUp();
+        });
+
+        // jQuery(devTrigger).mouseleave(function () {
+        //     jQuery('.devShortcuts').removeClass('active').addClass('inactive').slideUp();
+        // });
+
+
+
         if (inactive == 'inactive') {
             jQuery(devHelperPr).addClass('inactive').removeClass('active');
+            toggleCloseDH.slideUp();
+            toggleOpenDH.slideDown();
         } else {
             jQuery(devHelperPr).addClass('active').removeClass('inactive');
         }
+
+
+
         // close workspace
         jQuery(toggleCloseDH).click(function () {
             jQuery(devShortcuts).slideUp();
@@ -38,6 +97,8 @@ window.addEventListener('DOMContentLoaded', (event) => {
             jQuery(devHelperPr).addClass('inactive').removeClass('active');
             localStorage.setItem('devHelperPr', 'inactive');
         });
+
+
         // open workspace
         jQuery(toggleOpenDH).click(function () {
             jQuery(this).slideUp();
@@ -46,8 +107,8 @@ window.addEventListener('DOMContentLoaded', (event) => {
             localStorage.setItem('devHelperPr', 'active');
         });
 
-        
 
-        
+
+
     });
 });
